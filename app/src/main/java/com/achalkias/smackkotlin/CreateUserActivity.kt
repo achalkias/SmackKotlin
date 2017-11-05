c62d76c73fc80ec646e96f3cce1a97541cfe1abc
achalkias <achalkias@hotmail.com>
2017-11-05 11:40:02 +0200

    Create User Activity ui created

---
+++ app/src/main/java/com/achalkias/smackkotlin/CreateUserActivity.kt
@@ -0,0 +1,27 @@
+package com.achalkias.smackkotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun createUserBtnClicked(view: View) {
+
+    }
+
+    fun backgroundColorBtnClicked(view: View) {
+
+    }
+
+
+}
